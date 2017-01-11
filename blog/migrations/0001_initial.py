1194e8a407b7f8a834cdf735e56b7f3c27d78f08
injetlee <liyingjie26@hotmail.com>
2017-01-11 23:23:28 +0800

    add .gitignore

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('title', models.CharField(max_length=250)),
+                ('slug', models.SlugField(max_length=250, unique_for_date='publish')),
+                ('body', models.TextField()),
+                ('publish', models.DateTimeField(default=django.utils.timezone.now)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('status', models.CharField(default='draft', max_length=10, choices=[('draft', 'Draft'), ('published', 'Published')])),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='blog_posts')),
+            ],
+            options={
+                'ordering': ('-publish',),
+            },
+        ),
+    ]
--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+    status = models.CharField(max_length=10,choices=STATUS_CHOICE,default='draft')
     
 
     class Meta:
