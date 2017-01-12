8c5a70f86446b8623572070007a696a46f4ad04d
injetlee <liyingjie26@hotmail.com>
2017-01-12 19:52:38 +0800

    nothing

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 # Create your models here.
-
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
 class Post(models.Model):
     STATUS_CHOICE = (
         ('draft','Draft'),
@@ -16,9 +19,12 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,choices=STATUS_CHOICE,default='draft')
-    
+    objects = models.Manager()
+    published = PublishedManager() 
 
     class Meta:
         ordering = ('-publish',)
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.publish.year,self.publish.strftime('%m'),self.publish.strftime('%d'),self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    paginator = Paginator(posts,3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = Paginator.page(paginator.num_pages)
+    print(posts)
+    return render(request,'blog/post/list.html',{'page':page,'posts':posts})
+def post_detail(request,year,month,day,post):
+    post = get_object_or_404(Post,slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
+    return render(request,'blog/post/detail.html',{'post':post})
+
