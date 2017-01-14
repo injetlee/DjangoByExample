064a042682731d8fc2ed87ee81a0ea6cf438d949
injetlee <liyingjie26@hotmail.com>
2017-01-14 20:45:15 +0800

    class based view

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 # Create your views here.
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 def post_list(request):
     posts = Post.published.all()
-    paginator = Paginator(posts,3)
+    paginator = Paginator(posts, 3)
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -13,8 +20,10 @@ def post_list(request):
     except EmptyPage:
         posts = Paginator.page(paginator.num_pages)
     print(posts)
-    return render(request,'blog/post/list.html',{'page':page,'posts':posts})
-def post_detail(request,year,month,day,post):
-    post = get_object_or_404(Post,slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
-    return render(request,'blog/post/detail.html',{'post':post})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',
+                             publish__year=year, publish__month=month, publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
