8904e623a5d650ae9ea2e7ff3e4b40c1bf51eba2
injetlee <liyingjie26@hotmail.com>
2017-02-28 22:36:30 +0800

    exersice

--- website/bookmarks/account/views.py
+++ website/bookmarks/account/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 
 # Create your views here.
--- website/bookmarks/bookmarks/settings.py
+++ website/bookmarks/bookmarks/settings.py
@@ -101,3 +101,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+from django.core.urlresolvers import reverse_lazy
+
+LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
+LOGIN_URL = reverse_lazy('login')
+LOGOUT_URL = reverse_lazy('logout')
