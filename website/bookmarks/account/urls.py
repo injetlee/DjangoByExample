c265b4fd43e0c7ab6c6e3207eaf88d8142b82681
injetlee <liyingjie26@hotmail.com>
2017-03-01 23:10:14 +0800

    3.1

--- website/bookmarks/account/urls.py
+++ website/bookmarks/account/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     url(r'^logout-then-login/$',
         'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
     url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^password-change/$', 'django.contrib.auth.views.password_change',
+        name='password_change'),
+    url(r'^password-change/done/$',
+        'django.contrib.auth.views.password_change_done', name='password_change_done'),
 ]
